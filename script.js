50ae63345d84c0d09083035aa12e19dd28885365
unknown <bouyahyanada.solicode@gmail.com>
2022-01-21 15:02:13 +0100

    update

--- script.js
+++ script.js
@@ -11,11 +11,16 @@ function bringData(){
 }
 
 bringData();
-
 // fill html table from json file 
 
-function fillTable(dt){
-    dt.forEach(element => {
+function fillTable(re){
+    re.forEach(element => {
+            var ul = document.createElement("ul");
+            element.availablity.forEach(av => {
+                var li = document.createElement("li");
+                li.innerHTML = av;
+                ul.appendChild(li);
+            })
         var provider_info = element.provider["company name"] +'<br>'+ element.provider["adress"];
         // console.log(element);
         $('#Tbody').append($('<tr>')
@@ -23,7 +28,8 @@ function fillTable(dt){
         .append($('<td>').append(element.name))
         .append($('<td>').append(element.price))
         .append($('<td>').append(element.category))
-        .append($('<td>').append(element.availablity))
+        // .append($('<td>').append(element.availablity))
+        .append($('<td>').append(ul.innerHTML))
         .append($('<td>').append(provider_info))
     )});
 }
@@ -38,15 +44,44 @@ function fillTable(dt){
             list_search.push(prod);
         }
     });
-    // Tbody_content.innerHTML = "";
     $("#Tbody").html("");
     fillTable(list_search);
     // console.log(globalData[0].name.includes(mySearchBar.value))
 })
 
+// sort table by id and price
+function sort_byNum(re, arow){
+    var sorting = re.parentElement.innerText.trim();
+    if(arow == "up"){
+        globalData.sort(function(a,b){
+            return a[sorting] - b[sorting]
+        })
+    }
+    else if(arow=="down"){
+        globalData.sort(function(a,b){
+            return b[sorting] - a[sorting]
+        })
+    }
+    $("#Tbody").html("");
+    fillTable(globalData);
 
-// $(document).ready(function(){
-//     $(".sort_alph").on('click', function(){
-        
-//     })
-// })
+}
+
+
+//sort table by name and category
+function sort_alph(re, arow){
+    var sorting = re.parentElement.innerText.trim();
+    if (arow == "up"){
+        globalData.sort(function(a,b){
+            if(a[sorting] < b[sorting]) return -1
+        })
+    }
+    else{
+        globalData.sort(function(a,b){
+            if(a[sorting] > b[sorting]) return -1
+        })
+    }
+    $("#Tbody").html("");
+    fillTable(globalData);
+
+}
